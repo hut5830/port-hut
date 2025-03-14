35ef92105a96d83402613d7109f61f50674f5b00
tatchakorn.c@jmtnetwotk.co.th <watchrapon.w>
2025-03-15 02:59:07 +0700

    UpdateFirstStepClone

--- index.js
+++ index.js
@@ -2,8 +2,20 @@ const express = require(`express`);
 const app = express();
 const port = 8000;
 
-app.get(``, (req, res) => {
-    res.send('hrllo world');
+app.get(`/testapideploy`, (req, res) => {
+    res.send(
+        {
+            status: 200,
+            message: 'hello world Hut',
+            Name: [
+                {
+                    firstname: 'Tatchakorn',
+                    lastname: 'Chomyong',
+                    nickname: 'Hut',
+                }
+            ],
+        }
+    );
 })
 
 app.listen(port, () => {
