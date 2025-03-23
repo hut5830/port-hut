d4f72bf1cf1f307bf504260d0c027a80efc87b07
tatchakorn.c@jmtnetwotk.co.th <watchrapon.w>
2025-03-23 22:29:52 +0700

    'UpdateHeroku'

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require(`express`);
 const cors = require("cors");
 const fileupload = require('express-fileupload');
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 app.use(cors({
     origin: '*', // For testing (not recommended for production)
